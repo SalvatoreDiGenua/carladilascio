6e5de7065341e259057e81a6d7a606b69bc3d686
Salvatore Di Genua <saldigenua@gmail.com>
2026-09-12 00:31:54 +0200

    feat(home): enhance card component styling and add new color variables

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmCardImports } from '@spartan-ng/helm/card';
 import { SITE_CONTENT } from '../../core/data/site-content';
 import { Seo } from '../../core/seo/seo';
 import { CtaBannerComponent } from '../../shared/cta-banner/cta-banner';
@@ -9,7 +10,13 @@ import { AboutMenu } from '../../core/services/about-menu';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, TranslocoPipe, HlmButtonImports, CtaBannerComponent],
+  imports: [
+    RouterLink,
+    TranslocoPipe,
+    HlmButtonImports,
+    HlmCardImports,
+    CtaBannerComponent,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
